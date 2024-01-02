147d5b52962639703ca2d7cf37801c07c3c342cb
Ahan Bandyopadhyay <ahanbandyopadhyay@gmail.com>
2024-01-02 20:47:48 +0530

    overlay camera on background garphics

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ cap = cv2.VideoCapture(0)  # 0 indicates the default camera
 cap.set(3, 640)
 cap.set(4, 480)
 
+
+# *******************STEP 2: SETUP GRAPHICS***********************
+
+imgBackground = cv2.imread("Resources/background.png")
+
+
+while True:
+    success, img = cap.read()
+
+    imgBackground[162:162+480, 55:55+640] = img
+
+    cv2.imshow("Camera", img)
+    cv2.imshow("Face attendance", imgBackground)
+    cv2.waitKey(1)
