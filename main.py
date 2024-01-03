ad0ceb9b7494962caef7c374d13d86fe7a0206ba
Ahan Bandyopadhyay <ahanbandyopadhyay@gmail.com>
2024-01-03 10:52:52 +0530

    face recognition
    step initial commit

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # ******************STEP 1: Webcam setup************************
 import os
+import pickle
 
 import cv2
+import face_recognition
 
 # Open the webcam (the default camera)
 cap = cv2.VideoCapture(0)  # 0 indicates the default camera
@@ -26,9 +28,13 @@ for path in modePathList:
 print(len(imgModeList))
 
 # *********************STEP 3: ENCODING GENERATOR************************
+# go to EncodeGenerator.py
 
-
-
+# *********************STEP 4: FACE RECOGNITION************************
+file = open("EncodeFile.p", 'rb')
+encodeListKnownWithIDs = pickle.load(file)
+file.close()
+encodeListKnown, studentIDs = encodeListKnownWithIDs
 
 
 
@@ -36,9 +42,22 @@ print(len(imgModeList))
 while True:
     success, img = cap.read()
 
+    # RESIZING oUR IMAGES
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    faceCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
+
+    for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        print(matches)
+        print(faceDis)
+
     imgBackground[162:162 + 480, 55:55 + 640] = img
-    imgBackground[44:44+633, 808:808+414] = imgModeList[3]
+    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[3]
 
     # cv2.imshow("Camera", img)
-    cv2.imshow("Face attendance", imgBackground)
-    cv2.waitKey(1)
+    # cv2.imshow("Face attendance", imgBackground)
+    # cv2.waitKey(1)
