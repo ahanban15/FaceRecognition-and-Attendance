ee03a478379c08d84b1d1db9bede7d42b293c883
Ahan Bandyopadhyay <ahanbandyopadhyay@gmail.com>
2024-01-04 10:14:37 +0530

    how to fetch image n data realtime

--- main.py
+++ main.py
@@ -8,6 +8,21 @@ import cvzone
 import face_recognition
 import numpy as np
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+cred = credentials.Certificate(
+    "faceattendance-and-recognition-firebase-adminsdk-qxxb5-f986cd2945.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://faceattendance-and-recognition-default-rtdb.firebaseio.com/",
+    'storageBucket': "faceattendance-and-recognition.appspot.com"
+
+})
+
+bucket = storage.bucket()
+
 # Open the webcam (the default camera)
 cap = cv2.VideoCapture(0)  # 0 indicates the default camera
 
@@ -39,7 +54,18 @@ encodeListKnownWithIDs = pickle.load(file)
 file.close()
 encodeListKnown, studentIDs = encodeListKnownWithIDs
 
+# *******************Step 5: DATABASE SETUP ON GOOGLE FIREBASE***************
 
+# *******************Step 6: ADD DATA TO DATABASE from AddDataToDatabase.py***************
+
+# *******************Step 7: ADD IMAGES TO DATABASE from EncodeGenerator.py***************
+
+# *******************Step 8: REALTIME DATABASE UPDATE ****************************
+
+modeType = 0
+counter = 0
+id = -1
+imgStudent = []
 
 # final call to camera run
 while True:
@@ -51,6 +77,9 @@ while True:
 
     faceCurFrame = face_recognition.face_locations(imgS)
     encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
+
+    imgBackground[162:162 + 480, 55:55 + 640] = img
+    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
 
     for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
@@ -70,11 +99,58 @@ while True:
             y1, x2, y2, x1 = faceLoc
             y1, x2, y2, x1 = 4*y1, 4*x2, 4*y2, 4*x1
             bbox = 55+x1, 162+y1, x2-x1, y2-y1
-            cvzone.cornerRect(imgBackground, bbox, rt=0)
+            imgBackground = cvzone.cornerRect(imgBackground, bbox, rt=0)
 
-    imgBackground[162:162 + 480, 55:55 + 640] = img
-    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[3]
+            id = studentIDs[matchIndex]
 
-    # cv2.imshow("Camera", img)
-    # cv2.imshow("Face attendance", imgBackground)
-    # cv2.waitKey(1)
+            if counter == 0:
+                counter = 1
+                modeType = 1
+
+    if counter!=0:
+        # Get the data
+        if counter == 1:
+            studentInfo = db.reference(f'Students/{id}').get()
+            # print(studentInfo)
+
+            # Get the image
+            blob = bucket.get_blob(f'Images/{id}.png')
+            array = np.frombuffer(blob.download_as_string(), np.uint8)
+
+            imgStudent = cv2.imdecode(array, cv2.COLOR_BGRA2BGR)
+
+        # obtain image
+
+        cv2.putText(imgBackground, str(studentInfo['total_attendance']),(861, 125),
+        cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1)
+
+        cv2.putText(imgBackground, str(studentInfo['major']),(1006, 550),
+        cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1)
+
+        cv2.putText(imgBackground, str(studentInfo['id']),(1006, 493),
+        cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+
+        cv2.putText(imgBackground, str(studentInfo['cgpa']),(910, 625),
+        cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+
+        cv2.putText(imgBackground, str(studentInfo['year']),(1025, 625),
+        cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+
+        cv2.putText(imgBackground, str(studentInfo['starting_year']),(1125, 625),
+        cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+
+        (w, h), _ = cv2.getTextSize(studentInfo['name'], cv2.FONT_HERSHEY_COMPLEX, 1, 1)
+
+        offset = (414 - w)//2
+
+        cv2.putText(imgBackground, str(studentInfo['name']), (808+offset, 445),
+                    cv2.FONT_HERSHEY_COMPLEX, 1, (50, 50, 50), 1)
+
+        imgBackground[175:175+216, 909:909+216] = imgStudent
+
+        counter+=1
+
+
+    cv2.imshow("Camera", img)
+    cv2.imshow("Face attendance", imgBackground)
+    cv2.waitKey(1)
